83301234ace995a9e3328b3e638341dd947b5f35
DylanVallejo <dylan1811vallejo@gmail.com>
2024-03-24 22:04:18 -0500

    first commit proyecto gestor de productos y facturas,  roles ,seguridad ,generacion de pdfs, dashboard

---
+++ src/main/java/com/api/gestor/ApiGestorFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.api.gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestorFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestorFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gestor/ApiGestorFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestorFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
