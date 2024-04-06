f29873c42f3c4d3618ac2fddd618bad63d692f4d
DylanVallejo <dylan1811vallejo@gmail.com>
2024-04-05 20:42:52 -0500

    - se adiciona Enitdad Producto  y sus respectivos campos junto a la notacion ManyToOne par relacionar la tabla categoria

---
+++ src/main/java/com/api/gestor/pojo/Producto.java
@@ -0,0 +1,26 @@
+package com.api.gestor.pojo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name="producto")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="categoria_fk", nullable = false)
+    private Categoria categoria;
+
+}
