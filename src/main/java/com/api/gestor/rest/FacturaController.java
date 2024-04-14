04b7baa234432abb909a7be02ef654127d626975
DylanVallejo <dylan1811vallejo@gmail.com>
2024-04-13 23:59:38 -0500

    - se adiciona controlador

---
+++ src/main/java/com/api/gestor/rest/FacturaController.java
@@ -0,0 +1,10 @@
+package com.api.gestor.rest;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/factura")
+public class FacturaController {
+}
